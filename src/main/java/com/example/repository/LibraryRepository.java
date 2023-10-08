c2763d5e772312df1278da742d16049bc478cc8b
Nishida Tomoya <nishitomo7396@gmail.com>
2023-10-08 16:03:51 +0900

    lesson02実装完了

---
+++ src/main/java/com/example/repository/LibraryRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.enetity.Library;
+
+@Repository
+public interface LibraryRepository extends JpaRepository<Library, Integer> {
+	
+	// 必要に応じて機能を実装
+
+}
---
+++ src/main/java/com/example/service/LibraryService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.enetity.Library;
+import com.example.repository.LibraryRepository;
+
+@Service
+public class LibraryService {
+
+	private final LibraryRepository libraryRepository;
+	
+	// コンストラクタでインジェクション
+	@Autowired
+	public LibraryService(LibraryRepository libraryRepository) {
+		this.libraryRepository = libraryRepository;
+	}
+	
+	
+	// データ全件取得用メソッド
+	public List<Library> findAll() {
+		return this.libraryRepository.findAll();
+	}
+}
